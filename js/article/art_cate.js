74d16615d7122bada4e6ee00934540a212d2467c
qingyi <qingyi@qq.com>
2020-09-15 15:54:14 +0800

    'aa'

--- js/article/art_cate.js
+++ js/article/art_cate.js
@@ -4,7 +4,6 @@ $(function() {
 
   initArtCateList()
 
-  // 获取文章分类的列表
   function initArtCateList() {
     $.ajax({
       method: 'GET',
@@ -16,7 +15,6 @@ $(function() {
     })
   }
 
-  // 为添加类别按钮绑定点击事件
   var indexAdd = null
   $('#btnAddCate').on('click', function() {
     indexAdd = layer.open({
@@ -27,7 +25,6 @@ $(function() {
     })
   })
 
-  // 通过代理的形式，为 form-add 表单绑定 submit 事件
   $('body').on('submit', '#form-add', function(e) {
     e.preventDefault()
     $.ajax({
@@ -40,16 +37,13 @@ $(function() {
         }
         initArtCateList()
         layer.msg('新增分类成功！')
-        // 根据索引，关闭对应的弹出层
         layer.close(indexAdd)
       }
     })
   })
 
-  // 通过代理的形式，为 btn-edit 按钮绑定点击事件
   var indexEdit = null
   $('tbody').on('click', '.btn-edit', function() {
-    // 弹出一个修改文章分类信息的层
     indexEdit = layer.open({
       type: 1,
       area: ['500px', '250px'],
@@ -58,7 +52,6 @@ $(function() {
     })
 
     var id = $(this).attr('data-id')
-    // 发起请求获取对应分类的数据
     $.ajax({
       method: 'GET',
       url: '/my/article/cates/' + id,
@@ -68,7 +61,6 @@ $(function() {
     })
   })
 
-  // 通过代理的形式，为修改分类的表单绑定 submit 事件
   $('body').on('submit', '#form-edit', function(e) {
     e.preventDefault()
     $.ajax({
@@ -86,10 +78,8 @@ $(function() {
     })
   })
 
-  // 通过代理的形式，为删除按钮绑定点击事件
   $('tbody').on('click', '.btn-delete', function() {
     var id = $(this).attr('data-id')
-    // 提示用户是否要删除
     layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
       $.ajax({
         method: 'GET',
